c0ebf2f4c2a6508cf82fa1fa9e0ce49175f51630
zahrabateninia <zahrabatenin@gmail.com>
2025-02-24 19:42:32 +0330

    Write a script to monitor disk usage of a server, send email if threshold was exceeded

---
+++ monitorDiskUsage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# a script to monitor disk usage on a server and send an email notification if it exceeds a certain threshold
+
+THRESHOLD=90
+
+DISK_USAGE=$(df -h / | awk 'NR==2{print $5}' | sed 's/%//')
+
+if [ "$DISK_USAGE" -gt "$THRESHOLD" ]; then
+    echo "Warning: Disk usage on server $HOSTNAME has exceeded $THRESHOLD%." | mail -s "Disk space Alert" zahrabatenin@gmail.com
+    exit 1
+else
+    echo "Disk usage is below $THRESHOLD%."
+    exit 0
+fi
